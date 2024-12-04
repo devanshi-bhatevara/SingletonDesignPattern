a3ab9e9da354f99ac10610a6e09840aa6304eebb
devanshi.bhatevara <devanshi8730@gmail.com>
2024-12-04 12:31:30 +0530

    initial commit

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,85 @@
+﻿using SingletonDemo;
+
+
+////with sealed
+//Singleton employee = Singleton.GetInstance;
+//employee.PrintDetails("Hello Employee");
+//Singleton student = Singleton.GetInstance;
+//student.PrintDetails("Hello student");
+
+
+
+
+////without sealed
+//SingletonWithoutSealed employee2 = SingletonWithoutSealed.GetInstance;
+//employee2.PrintDetails("Hello Employee 2");
+//SingletonWithoutSealed student2 = SingletonWithoutSealed.GetInstance;
+//student2.PrintDetails("Hello student 2");
+//SingletonWithoutSealed employee3 = SingletonWithoutSealed.GetInstance;
+//employee3.PrintDetails("Hello Employee 3");
+//Console.WriteLine("------------------------------------------------------------------------");
+//SingletonWithoutSealed.DerivedClass derived = new SingletonWithoutSealed.DerivedClass();
+//derived.PrintDetails("Hello from derived without sealed");
+
+
+
+
+////with locks
+//Parallel.Invoke(
+//    () => PrintEmployeeDetails(),
+//    () => PrintStudentDetails()
+//    );
+//static void PrintEmployeeDetails()
+//{
+//    SingletonWithLocks employee = SingletonWithLocks.GetInstance;
+//    employee.PrintDetails("Hello Employee");
+//}
+
+//static void PrintStudentDetails()
+//{
+//    SingletonWithLocks student = SingletonWithLocks.GetInstance;
+//    student.PrintDetails("Hello student");
+//}
+
+
+
+////eager loading
+//Parallel.Invoke(
+//    () => PrintEmployeeDetailsEager(),
+//    () => PrintStudentDetailsEager()
+//    );
+//static void PrintEmployeeDetailsEager()
+//{
+//    SingletonEagerLoading employee = SingletonEagerLoading.GetInstance;
+//    employee.PrintDetails("Hello Employee");
+//}
+
+//static void PrintStudentDetailsEager()
+//{
+//    SingletonEagerLoading student = SingletonEagerLoading.GetInstance;
+//    student.PrintDetails("Hello student");
+//}
+
+
+
+////lazy initialization
+//Parallel.Invoke(
+//    () => PrintEmployeeDetailsLazy(),
+//    () => PrintStudentDetailsLazy()
+//    );
+//static void PrintEmployeeDetailsLazy()
+//{
+//    SingletonLazyInitialization employee = SingletonLazyInitialization.GetInstance;
+//    employee.PrintDetails("Hello Employee");
+//}
+
+//static void PrintStudentDetailsLazy()
+//{
+//    SingletonLazyInitialization student = SingletonLazyInitialization.GetInstance;
+//    student.PrintDetails("Hello student");
+//}
+
+
+
+Console.ReadLine();
+
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    //works perfectly fine in a single threaded app but not with multi threaded
+
+    //why sealed keyword??
+    public sealed class Singleton
+    {
+        private static int counter;
+
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //this is lazy initialization and it works fine in single threaded environment 
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        //private ctor ensures that object is not instantiated other than within the class itself
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    //if a simple class outside this class wil try to inherit this class it won't allow because of the private constructor then why bother using the sealed keyword
+    //but if a nested class does it will allow it
+    public class SingletonWithoutSealed
+    {
+        private static int counter;
+
+        private static SingletonWithoutSealed instance = null;
+        public static SingletonWithoutSealed GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonWithoutSealed();
+                return instance;
+            }
+        }
+        private SingletonWithoutSealed()
+        {
+            counter++;
+            Console.WriteLine("Counter {0}", counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public class DerivedClass : SingletonWithoutSealed
+        {
+
+        }
+    }
+
+    public sealed class SingletonWithLocks
+    {
+        private static int counter;
+
+        private static readonly object lockObject = new object();
+
+        private static SingletonWithLocks instance = null;
+        public static SingletonWithLocks GetInstance
+        {
+            get
+            {
+                //double check locking
+                if (instance == null)
+                {
+                    //locks are very expensive so we should avoid hitting locks everytime
+                    lock (lockObject)
+                    {
+                        //this is lazy initialization and it works fine in single threaded environment 
+                        if (instance == null)
+                            instance = new SingletonWithLocks();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        //private ctor ensures that object is not instantiated other than within the class itself
+        private SingletonWithLocks()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+    //it is default thread safe as CLR takes care of it
+    public sealed class SingletonEagerLoading
+    {
+        private static int counter;
+
+        private static readonly SingletonEagerLoading instance = new SingletonEagerLoading();
+        public static SingletonEagerLoading GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private SingletonEagerLoading()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    } 
+    
+
+    //lazy objects are by default thread safe
+    public sealed class SingletonLazyInitialization
+    {
+        private static int counter;
+
+        private static readonly Lazy<SingletonLazyInitialization> instance = new Lazy<SingletonLazyInitialization>(()=> new SingletonLazyInitialization());
+        public static SingletonLazyInitialization GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        private SingletonLazyInitialization()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
